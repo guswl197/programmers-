3c5cc3557116a2bc79488e235bd9e0f5de2b588d
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-04-13 20:27:21 +0900

    Create 신고 결과 받기.cpp

---
+++ level1/신고 결과 받기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+map<string, set<string>> mp1;
+map<string ,int> mp2; 
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    int len= id_list.size(); 
+    vector<int> answer(len,0);
+    
+    for(int i=0; i<report.size(); i++){
+        istringstream iss(report[i]);
+        string a,b; 
+        iss>>a>>b; 
+        mp1[a].insert(b); 
+    }    
+    
+    for(int i=0 ;i<id_list.size(); i++){
+        for(auto j : mp1[id_list[i]]){
+            mp2[j]++; 
+        }
+    }
+    
+    for(int i=0; i<id_list.size(); i++){
+        for(auto j: mp1[id_list[i]]){
+            if(mp2[j]>=k){
+                answer[i]++; 
+            }
+        }
+    }
+    
+    return answer;
+}
