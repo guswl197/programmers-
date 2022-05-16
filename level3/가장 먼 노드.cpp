faf765771c58e6ef982575838f24b5641fef5ecb
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-05-16 21:18:02 +0900

    Update 가장 먼 노드.cpp

--- level3/가장 먼 노드.cpp
+++ level3/가장 먼 노드.cpp
@@ -7,39 +7,35 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    vector<vector<int>> graph(n+1); 
-    vector<int> count(n+1, 0); 
-    vector<bool> visit(n+1, false); 
+    vector<vector<int>> graph(n+1);
     queue<int> q; 
+    vector<int> visit(n+1, 0); 
     
     for(int i=0; i<edge.size(); i++){
         graph[edge[i][0]].push_back(edge[i][1]); 
         graph[edge[i][1]].push_back(edge[i][0]); 
     }
     
-    q.push(1); 
-    visit[1]= true; 
-    
+    q.push(1);
+    visit[1]=1; 
     while(!q.empty()){
-        int node= q.front(); 
-        q.pop(); 
+        int x = q.front(); 
+        q.pop();
         
-        for(int i=0; i<graph[node].size(); i++){
-            if(!visit[graph[node][i]]){
-                int curcnt= count[node]+1; 
-                visit[graph[node][i]]=true; 
-                count[graph[node][i]]= curcnt; 
-                q.push(graph[node][i]); 
+        for(int i=0; i<graph[x].size(); i++){
+            if(!visit[graph[x][i]]){
+               visit[graph[x][i]]= visit[x]+1; 
+                q.push(graph[x][i]); 
             }
         }
     }
     
-    sort(count.begin(), count.end(), greater<int>()); 
-    for(auto cnt : count){
-        if(count[0]!= cnt ){
-            break;
+    sort(visit.begin(), visit.end(), greater<int>()); 
+    for(auto i : visit){
+        if(visit[0]== i){
+            answer++;
         }
-        answer++;
     }
+    
     return answer;
 }
