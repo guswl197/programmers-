7f3a31a57b0d7823c3a3a0adefd0e3d508300cfe
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-09-12 19:57:46 +0900

    Update 비밀지도.cpp

--- level1/비밀지도.cpp
+++ level1/비밀지도.cpp
@@ -1,25 +1,26 @@
-#include <string>
-#include <vector>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
     vector<string> answer;
     for(int i=0; i<n ; i++){
-        arr1[i]= arr1[i]|arr2[i]; 
-        string ans="";
-        for(int j=0; j<n; j++){
-            if(arr1[i] % 2 ==0){
-                ans= " "+ans; 
+        arr1[i]= arr1[i] | arr2[i]; 
+        string ans=""; 
+        for(int j=0; j<n ; j++){
+            if(arr1[i]%2==0){
+                ans=" "+ans ; 
             }
             else{
-                ans="#"+ ans; 
+                ans="#"+ans; 
             }
             arr1[i]= arr1[i]/2; 
         }
         
         answer.push_back(ans); 
     }
+    
     return answer;
 }
