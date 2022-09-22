d8dbf67b9b4398fbd160432ddd3ab5e5621a266d
d이h <80441723+guswl197@users.noreply.github.com>
2022-09-22 11:58:40 +0900

    Update 키패드 누르기.cpp

--- level1/키패드 누르기.cpp
+++ level1/키패드 누르기.cpp
@@ -1,83 +1,72 @@
-#include <string>
-#include <vector>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int phone[5][4]; 
-pair<int ,int > le;
-pair<int ,int> ri; 
-string answer = "";
-pair<int,int> d; 
+pair<int, int> left_loc; 
+pair<int, int> right_loc; 
+pair<int, int> number_loc;  
 
-pair<int,int> change(int number){
-
-    switch(number){
-        case 1:
-            return {1,1}; 
-        case 2:
-            return {1,2}; 
-        case 3:
-            return {1,3}; 
-        case 4:
-            return {2,1}; 
-        case 5:
-            return {2,2}; 
-        case 6:
-            return {2,3}; 
-        case 7:
-            return {3,1}; 
-        case 8:
-            return {3,2}; 
-        case 9:
-            return {3,3}; 
-        case 0:
-            return {4,2};     
+pair<int,int> change(int num){
+    
+    pair<int,int> p; 
+    
+    if(num == 0){
+        return {3,1};    
     }
-}
-
-void leftmove(){
-     answer+= 'L'; 
-     le.first=d.first; 
-     le.second=d.second; 
-}
-
-void rightmove(){
-      answer+= 'R'; 
-      ri.first=d.first; 
-      ri.second=d.second;
+    
+    int cnt=1; 
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(cnt == num){
+                return {i,j};        
+          } 
+            cnt++; 
+        }
+    }
+    
+    return {0,0}; 
 }
 
 string solution(vector<int> numbers, string hand) {
-    le.first= 4, le.second= 1; 
-    ri.first= 4, ri.second= 3; 
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    string answer = "";
+    
+    left_loc={3,0};  //L
+    right_loc={3,2}; //R 
     
     for(int i=0; i<numbers.size(); i++){
-        d= change(numbers[i]);
-        
-        if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7){
-            leftmove(); 
-            continue; 
+        if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
+            answer+='L'; 
+            left_loc= change(numbers[i]); 
         }
-        if(numbers[i]==3 || numbers[i]==6 || numbers[i]==9){
-            rightmove(); 
-            continue; 
+        else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
+            answer+='R'; 
+            right_loc= change(numbers[i]); 
         }
-          
-        int leftcnt= abs(le.first- d.first)+ abs(le.second- d.second);
-        int rightcnt= abs(ri.first- d.first)+ abs(ri.second- d.second);
-        
-        if(leftcnt > rightcnt){
-            rightmove(); 
-        }
-        else if(leftcnt < rightcnt){
-            leftmove(); 
-        }
-        else if(hand=="right"){ 
-            rightmove(); 
-        }
-        else{ //left 
-            leftmove(); 
+        else{
+            number_loc= change(numbers[i]); 
+            int Ldis= abs(left_loc.first- number_loc.first) + abs(left_loc.second- number_loc.second); 
+            int Rdis= abs(right_loc.first- number_loc.first) + abs(right_loc.second- number_loc.second); 
+            
+            if(Ldis < Rdis){
+                answer+='L';
+                left_loc= {number_loc.first, number_loc.second};
+            }
+            else if(Ldis > Rdis){
+                answer+='R';
+                right_loc= {number_loc.first, number_loc.second};
+            }
+            else{
+                if(hand == "right"){
+                    answer+='R';
+                    right_loc= {number_loc.first, number_loc.second};
+                }
+                else{
+                    answer+='L';
+                    left_loc= {number_loc.first, number_loc.second};
+                }
+            }
         }
     }
     
